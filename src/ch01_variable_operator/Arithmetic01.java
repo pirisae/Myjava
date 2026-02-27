0e73cffddb4e12b175facfb62ad00c7d93200130
bluesky <ehdgornldls@naver.com>
2026-02-27 15:50:43 +0900

    first commit

--- src/ch01_variable_operator/Arithmetic01.java
+++ src/ch01_variable_operator/Arithmetic01.java
@@ -9,6 +9,6 @@ public class Arithmetic01 {
         System.out.println("빼기 : " + (x - y));
         System.out.println("곱하기 : " + (x * y));
         System.out.println("나누기 : " + (x / y));
-        System.out.println("나눗셈 : " + (x % y));
+        System.out.println("나눗셈 : " + (x % y)); // 나머지
     }
 }
